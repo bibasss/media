1af9c9f2ee7a2d11da61787029f223afc1ee6064
bibasss <your-email@example.com>
2025-04-07 17:32:36 +0500

    Перезаписан проект TypeScript

--- TypeScript/vite.config.ts
+++ TypeScript/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // Папка для сборки, она должна совпадать с тем, что указано в `vercel.json`
-  }
-});
+  plugins: [react()],
+})
